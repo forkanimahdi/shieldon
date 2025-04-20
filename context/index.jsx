7896784f7a61fc0dd4c8c07dbdb58b230460f2d2
forkanimahdi <forkanimahdi@gmail.com>
2025-04-20 20:43:00 +0100

    first commit

---
+++ context/index.jsx
@@ -0,0 +1,47 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { useColorScheme } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const myContext = createContext()
+
+const AppProvider = ({ children }) => {
+
+    const darkMode = useColorScheme() == "dark"
+    const [urls, setUrls] = useState([])
+
+
+    const getOtpUrls = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem("urls");
+
+            setUrls(JSON.parse(jsonValue)) 
+
+        } catch (e) {
+            console.error('Error reading OTPs:', e);
+            return [];
+        }
+    };
+     
+    useEffect(() => {
+        getOtpUrls()
+    }, [])
+
+
+
+    const data = {
+        darkMode,
+        urls,
+        setUrls
+    }
+
+
+    return <myContext.Provider value={data}>{children}</myContext.Provider>
+
+};
+
+const useAppContext = () => useContext(myContext)
+
+export { AppProvider, myContext, useAppContext };
+
+
+
